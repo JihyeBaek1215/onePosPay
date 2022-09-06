200fb96db05f9121d49d246f15a98b962a00d0b4
jihye <jihyebaek1215@gmail.com>
2022-09-06 22:57:44 +0900

    copyProperties Target 변경

--- src/main/java/onepos/domain/Pay.java
+++ src/main/java/onepos/domain/Pay.java
@@ -191,12 +191,12 @@ public class Pay {
 
         if ("PaySucess".equals(payStatus)){
             Paid paid = new Paid();
-            BeanUtils.copyProperties(this, pay);
+            BeanUtils.copyProperties(this, paid);
             paid.publishAfterCommit(); // 카프카 발행
         }
         if ("PayFail".equals(payStatus)){
             Refunded refunded = new Refunded();
-            BeanUtils.copyProperties(this, pay);
+            BeanUtils.copyProperties(this, refunded);
             refunded.publishAfterCommit(); // 카프카 발행
         }
     }
